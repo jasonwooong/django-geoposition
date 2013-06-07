f324e3e436a5e0e41a64efdcd0784486e7276088
Jason Wong <jason@arbutus>
2013-06-07 02:07:13 -0700

    Disabled scroll wheel in MapOptions.

--- geoposition/static/geoposition/geoposition.js
+++ geoposition/static/geoposition/geoposition.js
@@ -74,7 +74,8 @@ if (jQuery != undefined) {
             mapLatLng = new google.maps.LatLng(latitude, longitude);
             mapOptions = $.extend({}, mapDefaults, {
                 'center': mapLatLng,
-                'zoom': latitude && longitude ? 15 : 1
+                'zoom': latitude && longitude ? 15 : 1,
+                'scrollwheel': false
             });
             map = new google.maps.Map($mapContainer.get(0), mapOptions);
             marker = new google.maps.Marker({
